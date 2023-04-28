d500860351b2d99df6d1bce244de44e49f018a2d
Mariano Gabriel Marco <mariano268@gmail.com>
2023-04-28 16:45:51 -0300

    Avance en el portfolio - Seccion experiencia creada para todos los dispositivos - Seccion educacion creada para todos los dispositivos - Seccion habilidades comenzada:     - ng-circles configurados y funcionando     - Logica para modificar los circles dependiendo del tamaño de la pantalla configurada     - Aun resta terminar de modificar estetica y completar informacion

--- PortFolio_Frontend/src/app/app.module.ts
+++ PortFolio_Frontend/src/app/app.module.ts
@@ -5,18 +5,36 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { SkillsComponent } from './components/skills/skills.component';
+
+// Import ng-circle-progress
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule{ }
---
+++ PortFolio_Frontend/src/app/components/skills/skills.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+// Modulo para el tamaño de la pantalla.
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { distinctUntilChanged, tap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.css']
+})
+export class SkillsComponent implements OnInit {
+
+  Breakpoints = Breakpoints;
+  currentBreakpoint:string = '';
+
+  readonly breakpoint$ = this.breakpointObserver
+    .observe(['(min-width: 368px)' , '(min-width: 460px)' , '(min-width: 768px)' , '(min-width: 992px)' , '(min-width: 1200px)'])
+    .pipe(
+      tap(value => console.log(value)),
+      distinctUntilChanged()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
+
+  ngOnInit(): void {
+    this.breakpoint$.subscribe(() =>
+      this.breakpointChanged()
+    );
+  }
+  private breakpointChanged() {
+    if(this.breakpointObserver.isMatched('(min-width: 1200px)')) {
+      this.currentBreakpoint = "1200px";
+    } else if(this.breakpointObserver.isMatched('(min-width: 992px)')) {
+      this.currentBreakpoint = "992px";
+    } else if(this.breakpointObserver.isMatched('(min-width: 768px)')) {
+      this.currentBreakpoint = "768px";
+    } else if(this.breakpointObserver.isMatched('(min-width: 460px)')) {
+      this.currentBreakpoint = "460px";
+    } else if(this.breakpointObserver.isMatched('(min-width: 368px)')) {
+      this.currentBreakpoint = "368px";
+    }
+  }
+}
